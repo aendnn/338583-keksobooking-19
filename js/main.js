e6de9f74461b352f6effb7ef2ba0b673c5ff0642
Anna <aendnn9@gmail.com>
2020-02-01 15:59:15 +0200

    Исправляет замечания наставника

--- js/main.js
+++ js/main.js
@@ -8,14 +8,15 @@ var TITLES = [
   'Lazybear Студия Chocolate N1',
   'Двухуровневые Апартаменты в центре Санкт-Петербурга',
   'Уютная квартира в центре Санкт-Петербурга',
-  'Cовременные апартаменты-студио с видом ID189'
+  'Cовременные апартаменты-студио'
 ];
 
 var DESCRIPTIONS = [
   'Чистая и уютная квартира оформлена в современном стиле',
   'Великолепные апартаменты-студио с современным дизайнерским интерьером в стиле лофт',
   'Удобное месторасположение и уникальный дизайн апартаментов подарят Вам незабываемые ощущения',
-  'Квартира укомплектована всем необходимым для комфортного отдыха', 'Чистая и уютная квартира оформлена в современном стиле',
+  'Квартира укомплектована всем необходимым для комфортного отдыха',
+  'Чистая и уютная квартира оформлена в современном стиле',
   'Современные апартаменты в центре Киева, оформленные в стиле лофт для удобного размещается 2 человек',
   'Дешевая, но все же уютная квартира в центре Киева',
   'Однокомнатная квартира, с высокими потолками'
@@ -84,7 +85,7 @@ var PIN = {
   yMin: 130,
   yMax: 630
 };
-var PIN_POINTER_CENTER = PIN.width / 2;
+var pinPointer = PIN.width / 2;
 
 var TOTAL_ADS = 8;
 var ads = [];
@@ -102,16 +103,17 @@ var getRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 };
 
-// возвращает случайный индекс элемента
-var getValue = function (array) {
+// возвращает случайный элемент
+var getRandomThing = function (array) {
   return array[getRandomNumber(0, array.length - 1)];
 };
 
-// возвращает массив преимуществ произвольной длины
-var getString = function (array) {
+// возвращает массив произвольной длины
+var getElements = function (array) {
+  var condition = getRandomNumber(1, array.length + 1); // кол-во итераций от 1(чтобы не возвращался пустой массив) до длины массива + 1(сдвиг индекса чтобы получить последний элемент)
   var newArray = [];
 
-  for (var i = 0; i <= getRandomNumber(1, array.length); i++) {
+  for (var i = 0; i < condition; i++) {
     newArray.push(array[i]);
   }
   return newArray;
@@ -120,27 +122,29 @@ var getString = function (array) {
 // возвращает массив с объявлениями
 var getAds = function (array, quantity) {
   for (var i = 0; i < quantity; i++) {
+    var locationX = getRandomNumber(0, (PIN.xMax - PIN.width - pinPointer + EXCLUDING_NUMBER));
+    var locationY = getRandomNumber(PIN.yMin, PIN.yMax + EXCLUDING_NUMBER - PIN.height);
+
     var ad = {
       author: {
         avatar: 'img/avatars/user' + '0' + (i + 1) + '.png'
       },
       location: {
-        x: getRandomNumber(0, (PIN.xMax - PIN.width) + EXCLUDING_NUMBER), // координата x от 0 до 1150(карта 1200px - маркер)
-        y: getRandomNumber(PIN.yMin, PIN.yMax + EXCLUDING_NUMBER)
+        x: locationX,
+        y: locationY
       },
       offer: {
-        title: getValue(TITLES),
-        address: // ad.location.x - PIN.width + ', ' + ad.location.y - PIN.height, выдает undefined
-        getRandomNumber(0, (PIN.xMax - PIN.width) + EXCLUDING_NUMBER) + ', ' + getRandomNumber(PIN.yMin, PIN.yMax + EXCLUDING_NUMBER),
-        price: getValue(PRICES),
-        type: getValue(TYPE_OF_HOUSES),
-        rooms: getValue(ROOMS),
-        guests: getValue(GUESTS),
-        checkin: getValue(CHECKIN_AND_CHECKOUT_HOURS),
-        checkout: getValue(CHECKIN_AND_CHECKOUT_HOURS),
-        features: getString(FEATURES_LIST),
-        description: getValue(DESCRIPTIONS),
-        photos: getString(PHOTOS_LIST)
+        title: getRandomThing(TITLES),
+        address: locationX + ', ' + locationY,
+        price: getRandomThing(PRICES),
+        type: getRandomThing(TYPE_OF_HOUSES),
+        rooms: getRandomThing(ROOMS),
+        guests: getRandomThing(GUESTS),
+        checkin: getRandomThing(CHECKIN_AND_CHECKOUT_HOURS),
+        checkout: getRandomThing(CHECKIN_AND_CHECKOUT_HOURS),
+        features: getElements(FEATURES_LIST),
+        description: getRandomThing(DESCRIPTIONS),
+        photos: getElements(PHOTOS_LIST)
       }
     };
     array.push(ad);
@@ -148,12 +152,13 @@ var getAds = function (array, quantity) {
   return array;
 };
 
+
 // возвращает отрисованную метку
 var renderPin = function (pin) {
   var pinElement = template.cloneNode(true);
 
-  pinElement.style.left = pin.location.x - PIN_POINTER_CENTER + 'px';
-  pinElement.style.top = pin.location.y - PIN.height + 'px';
+  pinElement.style.left = pin.location.x + 'px';
+  pinElement.style.top = pin.location.y + 'px';
   pinElement.querySelector('img').src = pin.author.avatar;
   pinElement.querySelector('img').alt = pin.offer.title;
 
@@ -168,10 +173,10 @@ var generatePins = function (array) {
     fragment.appendChild(renderPin(array[i]));
   }
 
-  map.appendChild(fragment);
+  map.querySelector('.map__pins').appendChild(fragment);
 };
 
 getAds(ads, TOTAL_ADS);
-generatePins(ads, TOTAL_ADS);
+generatePins(ads);
 
 map.classList.remove('map--faded');
